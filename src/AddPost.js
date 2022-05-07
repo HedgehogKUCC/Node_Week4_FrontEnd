efd31c2695326a2da29857d9a204e995ac7db292
HedgehogKU <ccc908925@gmail.com>
2022-05-07 16:01:38 +0800

    ADD: AddPost.js

--- src/AddPost.js
+++ src/AddPost.js
@@ -1,8 +1,33 @@
 import Header from "./Header";
+import UserFeature from "./UserFeature";
 
 function AddPost() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main className='addPost container'>
+                <div className="row">
+                    <div className="col-8">
+                        <div className='addPost__title'>
+                            張貼動態
+                            <div className='border'></div>
+                        </div>
+                        <div className="addPost__content">
+                            <form>
+                                <div className='title'>貼文內容</div>
+                                <textarea className='form-control' placeholder='輸入您的貼文內容'></textarea>
+                                <div className='upload'>上傳圖片</div>
+                                <div className='image'></div>
+                                <div className='submit'>送出貼文</div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-4">
+                        <UserFeature />
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
 
